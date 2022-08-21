04f3dccdf1175ebc45c45f41079dc36a7f42cdca
ozodbek <111683449+ozodbekmahamatjonov@users.noreply.github.com>
2022-08-21 15:17:28 +0500

    Add files via upload

---
+++ javascript2.js
@@ -0,0 +1,28 @@
+"use strict";
+
+console.log('Hello world');
+console.log('Script fileni ichidamiz');
+
+const MY_AGE = 22;
+alert(MY_AGE);
+
+let $name = 'Olim';
+alert($name);
+
+let $  = 12345;
+alert($);
+
+// bigInt
+const bigInt = 12345678901234567890123456789n;
+
+// true, false
+
+let age = null;
+alert(age);
+
+alert('Hello World');
+
+let name = prompt('ismingizni kiriting! ')
+alert(name);
+let answer = confirm('Bu Kompaniyadan mahsulotlarni sotib olmoqchimisiz?')
+alert(answer);
